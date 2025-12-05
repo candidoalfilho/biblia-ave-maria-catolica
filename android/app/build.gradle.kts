404ce6a10556a8a563504c3200080808279e9665
Cândido Alfredo <candidoalfilho@hotmail.com>
2025-12-05 17:16:22 -0300

    Improvements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.candido.biblia_ave_maria_catolica"
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
